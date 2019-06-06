218df0789a88cc9a694bbcc30a3247cc89eff479
periyandavart <51352174+periyandavart@users.noreply.github.com>
2019-06-06 09:57:06 +0530

    Create pos or neg.py

---
+++ pos or neg.py
@@ -0,0 +1,10 @@
+print("Enter the number")
+x=input()
+if x>0:
+    print("The number is positive")
+elif x<0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+    
+    
